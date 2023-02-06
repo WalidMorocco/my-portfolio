41d6c998b2208d116f2f3d6ab21a9a7baddb150b
Walid Amar <walidamarpro@gmail.com>
2023-02-06 15:54:21 -0500

    fix png

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Landing from "./components/AboutMe";
+import DotGroup from "./components/DotGroup";
 import Navbar from "./components/Navbar";
 import useMediaQuery from "./hooks/useMediaQuery";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState('home');
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) setIsTopOfPage(true);
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [])
 
   return (
     <div className="app bg-deep-blue">
-      <Navbar selectedPage={selectedPage} setSelectedPage={selectedPage}/>
-
+      <Navbar 
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage} 
+        setSelectedPage={selectedPage}
+      />
+      <div className="w-5/6 mx-auto md:h-full">
+        {isAboveMediumScreens && (
+          <DotGroup
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}/>
+        )}
+      </div>
+      <Landing setSelectedPage={setSelectedPage} />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,9 @@ const Link = ({ page, selectedPage, setSelectedPage }) => {
   const lowerCasePage = page.toLowerCase();
   return (
     <AnchorLink
-      className={`${selectedPage === lowerCasePage ? 'text-yellow' : ''}
-        hover: text-yellow transition duration-500`}
+      className={`${
+        selectedPage === lowerCasePage ? 'text-yellow' : ''
+      } hover:text-yellow transition duration-300`}
       href={`#${lowerCasePage}`}
       onClick={() => setSelectedPage(lowerCasePage)}
     >
@@ -16,26 +17,87 @@ const Link = ({ page, selectedPage, setSelectedPage }) => {
   );
 };
 
-const Navbar = ({ selectedPage, setSelectedPage }) => {
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
   const [isMenuToggled, setIsMenuToggled] = useState(false);
   const isAboveSmallScreens = useMediaQuery('(min-width: 768px)');
+  const navbarBackground = isTopOfPage ? '' : 'bg-purple';
 
   return (
     <nav className={'z-40 w-full fixed top-0 py-6'}>
       <div className='flex items-center justify-between mx-auto w-5/6'>
-        <h4 className='font-playfair text-3xl font-bold'>WA</h4>
+        <div className='w-1/3'>
+          <h4 className='font-playfair text-3xl font-bold'>Walid Amar</h4>
+        </div>
 
         {/* {Desktop View} */}
         {isAboveSmallScreens ? (
-          <div className='flex justify-between gap-16 font-sourcesanspro text-sm font-semibold'>
+          <div className='flex justify-between gap-16 font-sourcesanspro text-md font-semibold'>
             <Link
               page='Home'
               selectedPage={selectedPage}
               setSelectedPage={setSelectedPage}
             />
+            <Link
+              page='Skills'
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page='Projects'
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page='Contact'
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
           </div>
         ) : (
-          <div></div>
+          <button
+            className='rounded-full bg-purple p-2 w-1/6'
+            onClick={() => setIsMenuToggled(!isMenuToggled)}
+          >
+            <img
+              alt='menu-icon'
+              src='../assets/menu-icon.svg'
+            />
+          </button>
+        )}
+
+        {/* Mobile Menu Popup */}
+        {!isAboveSmallScreens && isMenuToggled && (
+          <div className='fixed right-0 bottom-0 h-full bg-blue w-1/2'>
+            {/* Close Icon */}
+            <div className='flex justify-end p-12'>
+              <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                <h1 className='text-3xl'>X</h1>
+              </button>
+            </div>
+            {/* Menu Items */}
+            <div className='flex flex-col gap-10 ml-[33%] text-2xl text-white'>
+              <Link
+                page='Home'
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page='Skills'
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page='Projects'
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page='Contact'
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+            </div>
+          </div>
         )}
       </div>
     </nav>
